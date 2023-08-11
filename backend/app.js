4a24b558aa9c916dac87d4219fcfa58a30b6ae8d
ZRomanova <romanova.zoya.2002@mail.ru>
2023-08-12 09:21:14 +1100

    4

--- backend/app.js
+++ backend/app.js
@@ -19,12 +19,26 @@ app.use('/uploads', express.static('uploads'))
 app.use('/downloads', express.static(path.resolve( __dirname, 'processing', 'downloads')))
 
 app.use('/api', apiRouter);
-// app.get('/', (req, res) => {
-//   res.sendFile(
-//     path.resolve(
-//       __dirname, '..', 'index.html'
-//     )
-//   )
-// })
+
+
+if (process.env.NODE_ENV === 'production' || true) {
+  app.use(express.static(__dirname))
+
+  const client = [
+    '.js',
+    '.css',
+    '.ico'
+  ];
+ 
+  app.get('*', (req, res) => {
+    if (!(req.path).includes('uploads/')) {
+      if (client.includes(path.extname(req.path))) {
+        res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'frontend', req.path));
+      } else {
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'frontend', 'index.html'))
+      }
+    }
+  })
+}
 
 module.exports = app
